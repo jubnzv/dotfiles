78ecb8cb71488f7d3b1fa252644b7b5fe72396be
Georgy Komarov <jubnzv@gmail.com>
2017-09-25 05:31:51 +0300

    Update i3 settings.

--- .local/scripts/dual.sh
+++ .local/scripts/dual.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Workstation setup
-xrandr --output VGA-1 --right-of LVDS-1 --auto
-xrandr --output HDMI-3 --left-of LVDS-1 --auto
+xrandr --output VGA1 --left-of LVDS1 --auto
+xrandr --output HDMI3 --right-of LVDS1 --auto
