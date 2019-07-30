1b498935234a24484eab2c287b5f58926f6d829b
Georgy Komarov <jubnzv@gmail.com>
2019-07-30 17:01:36 +0300

    Remove emacs configuration

--- emacs/.emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-;;; init.el --- Emacs main configuration file
-;;; Commentary:
-;;; Emacs config by Georgy Komarov
-;;; Code:
-
-(org-babel-load-file "~/.emacs.d/config.org")
-
-(provide 'init)
-;;; init.el ends here
--- emacs/.emacs.d/lisp/jbz-cc-mode.el
+++
@@ -1,8 +0,0 @@
-(defun jbz-cc-find-in-function ()
-    "Find string in function body."
-    (interactive)
-    (c-beginning-of-defun)
-    (push-mark)
-    (c-end-of-defun))
-
-(provide 'jbz-cc-mode)
