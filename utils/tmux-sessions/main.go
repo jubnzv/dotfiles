b00508aa6cff7704213a41842d4c3f935e2d5a0e
Georgy Komarov <jubnzv@gmail.com>
2019-08-09 13:17:24 +0300

    tmux: Advances sessions saving
    + Show icon if session configuration differs from saved one.
    + Keybind to save

--- utils/tmux-sessions/main.go
+++ utils/tmux-sessions/main.go
@@ -4,12 +4,13 @@
 package main
 
 import (
-	// "errors"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"os/user"
 	"path/filepath"
+	"reflect"
 	"strings"
 
 	tmux "github.com/jubnzv/go-tmux"
@@ -17,8 +18,14 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// Default path to store session configurations
+var DEFAULT_PATH = "~/.tmux/sessions/"
+
+// Message to print in stoud when session differs
+var DIFF_MESSAGE = " "
+
 const (
-	version = "1.0.0"
+	version = "1.1.0"
 )
 
 var (
@@ -31,6 +38,10 @@ var (
 
 	load     = app.Command("load", "Load tmux configuration from a yaml file")
 	loadPath = load.Arg("path", "Path to yaml configuration").String()
+
+	diff          = app.Command("diff", "Check that the current session is different from saved yaml configuration.")
+	diffPath      = diff.Arg("path", "Path to yaml configuration").String()
+	print_message = diff.Flag("print-message", "Print message in stdout when sessions are different").Bool()
 )
 
 // Represents a tmux window configuration
@@ -64,8 +75,10 @@ func getSessionNameFromFilepath(fpath string) string {
 	return session_name
 }
 
-// Saves current tmux session in yaml file
-func doSave(savePath string, fileName string, verbose bool) error {
+// Read active tmux session configuration
+func readCurrentSessionConf(filename string) (SessionConf, error) {
+	session_conf := SessionConf{}
+
 	// if !tmux.IsInsideTmux() {
 	//     return errors.New("Not inside tmux.")
 	// }
@@ -73,17 +86,17 @@ func doSave(savePath string, fileName string, verbose bool) error {
 	// Get current session configuration
 	session_name, err := tmux.GetAttachedSessionName()
 	if err != nil {
-		return err
+		return session_conf, err
 	}
 	session := tmux.Session{Name: session_name}
 	windows, err := session.ListWindows()
 	if err != nil {
-		return err
+		return session_conf, err
 	}
 
 	// Generate filename if not specified
-	if len(fileName) == 0 {
-		fileName = session_name
+	if len(filename) == 0 {
+		filename = session_name
 	}
 
 	// Collect windows configurations
@@ -92,13 +105,66 @@ func doSave(savePath string, fileName string, verbose bool) error {
 		windows_conf = append(windows_conf, WindowConf{
 			WindowName:     w.Name,
 			StartDirectory: w.StartDirectory})
-		fmt.Println(w.StartDirectory)
 	}
 
 	// Prepare session configuration
-	session_conf := SessionConf{
-		SessionName: fileName,
+	session_conf = SessionConf{
+		SessionName: filename,
 		Windows:     windows_conf}
+
+	return session_conf, nil
+}
+
+// Reads SessionConf object from a yaml file.
+func readSessionConfFromFile(filepath string) (SessionConf, error) {
+	session_conf := SessionConf{}
+
+	// Parse configuration
+	file_content, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return session_conf, err
+	}
+
+	err = yaml.Unmarshal(file_content, &session_conf)
+	if err != nil {
+		return session_conf, err
+	}
+
+	return session_conf, nil
+}
+
+// Reads go-tmux Session object from a yaml file.
+func readSessionFromFile(filepath string) (tmux.Session, error) {
+	session := tmux.Session{}
+
+	conf_in, err := readSessionConfFromFile(filepath)
+	if err != nil {
+		return session, err
+	}
+
+	// Prepare go-tmux session
+	session = tmux.Session{Name: conf_in.SessionName}
+	for idx, w := range conf_in.Windows {
+		session.AddWindow(tmux.Window{
+			Name: w.WindowName,
+			// TODO: Read base-index from .tmux.conf
+			Id:             idx + 1,
+			SessionId:      session.Id,
+			SessionName:    session.Name,
+			StartDirectory: w.StartDirectory,
+		})
+	}
+
+	return session, nil
+}
+
+// Saves current tmux session in yaml file
+func DoSave(savePath string, filename string, verbose bool) error {
+	// Get current session configuration
+	session_conf, err := readCurrentSessionConf(filename)
+	if err != nil {
+		return err
+	}
 
 	conf_out, err := yaml.Marshal(&session_conf)
 	if err != nil {
@@ -116,36 +182,17 @@ func doSave(savePath string, fileName string, verbose bool) error {
 	}
 
 	if verbose {
-		fmt.Printf("Saved in %s", savePath)
+		fmt.Printf("Saved in %s\n", savePath)
 	}
 
 	return nil
 }
 
 // Loads tmux session from json configuration file and attach to it.
-func doLoad(loadPath string) error {
-	// Parse configuration
-	file_content, err := ioutil.ReadFile(loadPath)
+func DoLoad(loadPath string) error {
+	session, err := readSessionFromFile(loadPath)
 	if err != nil {
 		return err
-	}
-	conf_in := SessionConf{}
-	err = yaml.Unmarshal(file_content, &conf_in)
-	if err != nil {
-		return err
-	}
-
-	// Prepare go-tmux session
-	session := tmux.Session{Name: conf_in.SessionName}
-	for idx, w := range conf_in.Windows {
-		session.AddWindow(tmux.Window{
-			Name:           w.WindowName,
-            // TODO: Read base-index from .tmux.conf
-			Id:             idx + 1,
-			SessionId:      session.Id,
-			SessionName:    session.Name,
-			StartDirectory: w.StartDirectory,
-		})
 	}
 
 	// Create new session
@@ -173,6 +220,58 @@ func doLoad(loadPath string) error {
 	return nil
 }
 
+// Checks that current session is different from saved yaml configuration
+// Returns 0 if sessions are same, -1 otherwise.
+func DoDiff(filepath string, verbose bool) (int, error) {
+	rc := 0
+
+	if !tmux.IsInsideTmux() {
+		return -1, errors.New("Not in tmux.")
+	}
+
+	// Get information about current session
+	session_conf_current, err := readCurrentSessionConf(filepath)
+	if err != nil {
+		return -1, err
+	}
+
+	// Generate filename if not specified
+	if len(filepath) == 0 {
+		session_name := session_conf_current.SessionName
+		filepath = fmt.Sprintf("%s/%s.yaml", DEFAULT_PATH, session_name)
+		filepath, err = expandTilde(filepath)
+		if err != nil {
+			return -1, err
+		}
+	}
+
+	// Session was never saved before
+	if _, err := os.Stat(filepath); os.IsNotExist(err) {
+		if verbose {
+			fmt.Printf("Can't open configuration file %s\n", filepath)
+		}
+		return -1, nil
+	}
+
+	// Read existing configuration file
+	session_conf_file, err := readSessionConfFromFile(filepath)
+	if err != nil {
+		return -1, err
+	}
+
+	// Compare SessionConf objects
+	if !reflect.DeepEqual(session_conf_current, session_conf_file) {
+		rc = -1
+		if verbose {
+			fmt.Println("Sessions are differs")
+		}
+	} else if verbose {
+		fmt.Println("Sessions are same")
+	}
+
+	return rc, nil
+}
+
 func main() {
 	rc := 0
 	var err error
@@ -186,13 +285,13 @@ func main() {
 				fmt.Printf("%s", err)
 				os.Exit(1)
 			}
-			path_to_save = fmt.Sprintf("~/.tmux/sessions/%s.yaml", session_name)
+			path_to_save = fmt.Sprintf("%s/%s.yaml", DEFAULT_PATH, session_name)
 		} else {
 			path_to_save = *savePath
 		}
 
 		// Perform saving
-		err = doSave(path_to_save, *fileName, *verbose)
+		err = DoSave(path_to_save, *fileName, *verbose)
 		if err != nil {
 			fmt.Printf("%s", err)
 			os.Exit(1)
@@ -200,10 +299,21 @@ func main() {
 
 	case load.FullCommand():
 		// Perform loading
-		err = doLoad(*loadPath)
+		err = DoLoad(*loadPath)
 		if err != nil {
 			fmt.Printf("%s", err)
 			os.Exit(1)
+		}
+
+	case diff.FullCommand():
+		// Check difference
+		rc, err = DoDiff(*diffPath, *verbose)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+		if (rc != 0) && (*print_message) {
+			fmt.Printf("%s\n", DIFF_MESSAGE)
 		}
 	}
 
