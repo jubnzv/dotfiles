ba61a121aa7de071423a5f6e183cbcdec9fff939
Georgiy Komarov <jubnzv@gmail.com>
2021-08-03 07:50:25 +0300

    Remove some deprecated files

--- .config/nvim/lua/lsp.lua
+++
@@ -1,9 +0,0 @@
--- local lsp_status = require('lsp-status')
-
-local M = {}
-
-function M.setup()
-
-end
-
-return M
--- .config/nvim/lua/nvim-colorizer.lua
+++
@@ -1,7 +0,0 @@
-local api = vim.api
-local M = {}
-
-function M.setup()
-end
-
-return M
